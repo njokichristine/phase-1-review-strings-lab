618157ed10a4f3b110b82f2c101d539e4d215a1e
njokichristine <christinenjoki6900@gmail.com>
2022-11-23 14:48:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+const currentUser= "christine njoki";
+const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`;
+console.log(currentUser);
+console.log(welcomeMessage);
+
+const upperCaseCurrentUser= currentUser.toUpperCase();
+const excitedWelcomeMessage= `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+console.log(upperCaseCurrentUser);
+console.log(excitedWelcomeMessage);
+
+const shortGreeting= `Welcome, ${currentUser.slice(0, 1)}!`;
+console.log(shortGreeting)
